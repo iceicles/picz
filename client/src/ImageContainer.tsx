4397920f75309d72b15e58e5cc5fd0e9f06f2c55
oshiogifted <oshiogifted2011@gmail.com>
2024-12-16 22:13:30 -0500

    changed vite start script cmd and minor style logic

--- client/src/ImageContainer.tsx
+++ client/src/ImageContainer.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
 export const ImageContainer = () => {
+  const noImages = true;
+  // apply styles if no images are available
+  const noImagesStyle = 'justify-items-center content-center';
   return (
     <>
-      <section className='imageContainer w-full h-[80vh] justify-items-center content-center'>
-        <span className='block justify-items-center content-center min-h-full'>
+      <section
+        className={`imageContainer w-full h-[80vh] ${
+          noImages ? noImagesStyle : ''
+        }`}
+      >
+        <div className={noImagesStyle}>
           Nothing for now. Upload an image you want public 😇{' '}
-        </span>
+        </div>
       </section>
     </>
   );
