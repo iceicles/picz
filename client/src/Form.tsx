c15c8fba588cb00f256307f848faa4dfb535723b
oshiogifted <oshiogifted2011@gmail.com>
2024-12-17 22:29:14 -0500

    handling post request for sending image file to server

--- client/src/Form.tsx
+++ client/src/Form.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { API_URL } from './constants';
 
 export const Form = () => {
@@ -7,6 +7,33 @@ export const Form = () => {
     const res = await fetch(API_URL + 'albums');
     const data = await res.json();
     console.log(data);
+  };
+
+  const onChangeHandler = async (e) => {
+    const imageFile = e.target.files[0];
+
+    const formData = new FormData();
+    // server requires image property from req.files (req.files.image)
+    formData.append('image', imageFile);
+
+    try {
+      const res = await fetch(API_URL + 'albums/uploads', {
+        method: 'POST',
+        body: formData,
+      });
+      // if the response is not OK, throw an error with the server's response
+      if (!res.ok) {
+        const errorData = await res.json(); // get the error response
+        throw new Error(JSON.stringify(errorData)); // throw the error as a stringified object
+      }
+
+      const data = await res.json();
+      console.log('data - ', data);
+    } catch (error) {
+      // parse the stringified error to access the original error properties
+      const errorResponse = JSON.parse(error.message);
+      console.log('Error in POST request - ', errorResponse.msg); // Now you can access `msg`
+    }
   };
 
   return (
@@ -24,7 +51,7 @@ export const Form = () => {
             <i className='text-red-400'>
               *Upload an image with size less than 1MB
             </i>
-            <input type='file' name='image' />
+            <input type='file' name='image' onChange={onChangeHandler} />
           </label>
           <label>
             <input type='submit' />
