12ba9f3d79803d52fe1580a88c26548428186e54
oshiogifted <oshiogifted2011@gmail.com>
2024-12-19 15:48:00 -0500

    adding timestamps to album schema

--- server/models/Album.js
+++ server/models/Album.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 
-const AlbumSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, 'Please provide a title'],
+const AlbumSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Please provide a title'],
+    },
+    image: {
+      type: String,
+      required: [true, 'Please provide an image'],
+    },
   },
-  image: {
-    type: String,
-    required: [true, 'Please provide an image'],
-  },
-});
+  // createdAt and updatedAt properties automatically added by mongoose
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Album', AlbumSchema);
