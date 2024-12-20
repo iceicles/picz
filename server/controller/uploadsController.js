860a332d191982fc95e53da8592a6a7d6d198dc9
oshiogifted <oshiogifted2011@gmail.com>
2024-12-20 17:38:03 -0500

    creating public assets folder if it doesn't exist

--- server/controller/uploadsController.js
+++ server/controller/uploadsController.js
@@ -1,16 +1,30 @@
 const CustomError = require('../errors');
 const path = require('path');
+const fs = require('fs');
 
 /* this will upload the images to the local server
     note: this increase server code size
 */
 const uploadAlbumImageLocal = async (req, res) => {
+  // not sending json in req.body from client
+  const productImage = req.files.image;
+
+  // create public/uploads folder if it doesn't exist
+  const publicAssetsFolder = path.join(__dirname, '../public/uploads');
+  let imagePath;
+  try {
+    if (!fs.existsSync(publicAssetsFolder)) {
+      fs.mkdirSync(publicAssetsFolder, { recursive: true });
+    }
+    imagePath = path.join(publicAssetsFolder, productImage.name);
+  } catch (error) {
+    console.log(error);
+  }
+
   // check if file exists
   if (!req.files) {
     throw new CustomError.BadRequestError('No File Uploaded');
   }
-
-  const productImage = req.files.image;
 
   // check format
   if (!productImage.mimetype.startsWith('image')) {
@@ -24,11 +38,6 @@ const uploadAlbumImageLocal = async (req, res) => {
       'Please upload image smaller than 1MB'
     );
   }
-
-  const imagePath = path.join(
-    __dirname,
-    '../public/uploads/' + `${productImage.name}`
-  );
 
   // use 'mv' method provided by fileUpload pkg to move the file to /uploads folder
   await productImage.mv(imagePath);
