5c017fe24a46a5f922598faabcf15badcbbdd6d1
oshiogifted <oshiogifted2011@gmail.com>
2024-12-20 19:15:15 -0500

    restricted album title char length

--- server/models/Album.js
+++ server/models/Album.js
@@ -5,6 +5,8 @@ const AlbumSchema = new mongoose.Schema(
     title: {
       type: String,
       required: [true, 'Please provide a title'],
+      minlength: 5,
+      maxlength: 30,
     },
     image: {
       type: String,
